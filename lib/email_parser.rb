3e453ee54f01541cfe84a54af8b4b9520a2d9a87
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-26 23:22:07 -0500

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,24 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    attr_accessor :parse
+
+   def initialize(parse)
+    @parse = parse
+   end
+
+   def parse
+    @parse.split(/,?\s/).uniq
+   end
+end
+
+# EmailAddressParser
+#   #parse
+#     parses CSV emails
+#     parses space delimited emails
+#     parses both CSV and space delimited emails
+#     parses and removes duplicate emails
+
+# Finished in 0.00472 seconds (files took 0.18033 seconds to load)
+# 4 examples, 0 failures
